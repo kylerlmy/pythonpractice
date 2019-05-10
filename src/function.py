aabaefc97819b2030b1a0d7ba46b8217bd3ee78d
kyle <kylerlimy@gmail.com>
2019-05-10 18:09:51 +0800

    function

--- src/function.py
+++ src/function.py
@@ -10,12 +10,22 @@ say_hello() #call function again
 def print_max(a,b):
     if a>b:
         print('{} is maximum'.format(a))
-        say_hello()
     elif a==b:
+        say_hello()
         print('{} is equal to {}'.format(a,b))
     else:
         print('{} is maximum'.format(b) )
 
 
 print_max(8,5)
+
+x=50
+def func():
+    global x
+
+    print('x is:',x)
+    x=2
+    print('Changed global x to',x)
+func()
+print('Value of global x is',x)
 
